c9612c98b061256e15715cba1c2b5de1e0d847a9
Brais <lordprofesor@gmail.com>
2021-09-16 10:39:58 +0200

    Add simple ListView to ListViewActivity

--- app/src/main/java/com/example/learningkotlin/ListViewActivity.kt
+++ app/src/main/java/com/example/learningkotlin/ListViewActivity.kt
@@ -2,6 +2,7 @@ package com.example.learningkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.ListView
 
 class ListViewActivity : AppCompatActivity() {
@@ -11,7 +12,8 @@ class ListViewActivity : AppCompatActivity() {
 
         val listView = findViewById<ListView>(R.id.listViewUsers)
 
-        var myFirstUser = User("Brais", 32, "32789654", "Alemana",null )
+        //Array personalizado
+        /*var myFirstUser = User("Brais", 32, "32789654", "Alemana",null )
         var mySecondUser = User("Ramonin", 38, "32700600Z")
         var myThirdUser = User("Jose Luis", 54, "47896547X")
 
@@ -20,15 +22,16 @@ class ListViewActivity : AppCompatActivity() {
         userList.add(mySecondUser)
         userList.add(myThirdUser)
 
-        listView.adapter = MyUserAdapter(applicationContext, userList)
+        listView.adapter = MyUserAdapter(applicationContext, userList)*/
 
-
-
-
-
-
-
-
+        //Array estandar (sin personalizar)
+        var frutas = arrayListOf<String>()
+        frutas.add("Fresas")
+        frutas.add("Arándanos")
+        frutas.add("Kiwis")
+        frutas.add("Moras")
+        frutas.add("Frambuesas")
+        listView.adapter = ArrayAdapter<String>(applicationContext, android.R.layout.simple_list_item_1, frutas)
 
     }
 }
