bb66fb5cd92e5d35315a9d27c720b56396785de0
Brais <lordprofesor@gmail.com>
2021-12-20 13:10:36 +0100

    -Constructor of listView modificated -Added emptyView for our custom listView.

--- app/src/main/java/com/example/learningkotlin/ListViewActivity.kt
+++ app/src/main/java/com/example/learningkotlin/ListViewActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.TextView
 
 class ListViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,12 +18,20 @@ class ListViewActivity : AppCompatActivity() {
         var mySecondUser = User("Ramonin", 38, "32700600Z")
         var myThirdUser = User("Jose Luis", 54, "47896547X")
 
+
         var userList = arrayListOf<User>()
         userList.add(myFirstUser)
         userList.add(mySecondUser)
         userList.add(myThirdUser)
+        userList.add(myFirstUser)
+        userList.add(mySecondUser)
+        userList.add(myThirdUser)
+        userList.add(myFirstUser)
+        userList.add(mySecondUser)
+        userList.add(myThirdUser)
 
-        listView.adapter = MyUserAdapter(applicationContext, userList)
+        listView.adapter = MyUserAdapter(applicationContext, R.layout.item_list_view_user, userList)
+        listView.emptyView = findViewById<TextView>(R.id.emptyElement) //Aquí seteamos la vista que queremos que aparezca en caso de que el listado esté vacio.
 
         //Array estandar (sin personalizar)
      /*   var frutas = arrayListOf<String>()
--- app/src/main/java/com/example/learningkotlin/MyUserAdapter.kt
+++ app/src/main/java/com/example/learningkotlin/MyUserAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.learningkotlin
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,15 +9,19 @@ import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
 
-//El XML del item se puede pasar en el constructor de class MyUserAdapter. En vez de eso como en
-//este caso el XML va a ser siempre el mismo. Se lo paso directamente al constructor del ArrayAdapter.
-class MyUserAdapter(context: Context, private val userList: ArrayList<User>)
-    : ArrayAdapter<User>(context, R.layout.item_list_view_user, userList) {
+/*El XML del item se puede pasar en el constructor de class MyUserAdapter. En vez de eso como en
+este caso el XML va a ser siempre el mismo. Se lo paso directamente al constructor del ArrayAdapter.*/
+class MyUserAdapter(context: Context, viewToPaint: Int, private val userList: ArrayList<User>)
+    : ArrayAdapter<User>(context, viewToPaint, userList) {
 
-
+    /*Cada vez que se dibuja un elemento nuevo en pantalla se hace una llamada al getView. Por este motivo, es llamado la primera
+    que se muestra la vista para dibujar los elementos iniciales y posteriormente cada vez que hacemos scroll arriba o abajo y
+    aparecen y desaparecen nuevos elementos.*/
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        /*En principio solo usaremos el parametro position. Por comodidad dejaremos el convertView y el parent para más adelante.
+        Estos nos serviran para optimizar el funcionamiento del listView con los viewHolder, pero por ahora, podremos prescindir de ellos*/
+        Log.i("Demostración: ","entrando en el getView")
 
-        //CUIDADO AQUI HAY QUE DARLE UNA REVISION PORQUE NO TENGO CLARO SI USAR EL PARAMETRO CONVERTVIEW!!!!!!!!!!!!!!!!
         val inflater = LayoutInflater.from(context)
         val currentListItem = inflater.inflate(R.layout.item_list_view_user, null)
 
@@ -30,5 +35,4 @@ class MyUserAdapter(context: Context, private val userList: ArrayList<User>)
 
         return currentListItem
     }
-
 }
