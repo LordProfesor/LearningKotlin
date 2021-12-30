f1daa14f282a820ec083dd98668d8702734680b9
Brais <lordprofesor@gmail.com>
2021-12-30 13:58:40 +0100

    -Add emptyElement to customListVIew -Add OnItemClickListener to customListView.

--- app/src/main/java/com/example/learningkotlin/ListViewActivity.kt
+++ app/src/main/java/com/example/learningkotlin/ListViewActivity.kt
@@ -5,13 +5,16 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.TextView
+import android.widget.Toast
 
 class ListViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view)
 
+        //Aqui controlamos que si el ListView está vacio, se muestre un elemento alternativo que infome al usuario de lo que está ócurriendo
         val listView = findViewById<ListView>(R.id.listViewUsers)
+        listView.emptyView = findViewById(R.id.emptyElement)
 
         //Array personalizado
         var myFirstUser = User("Brais", 32, "32789654", "Alemana",null )
@@ -30,8 +33,16 @@ class ListViewActivity : AppCompatActivity() {
         userList.add(mySecondUser)
         userList.add(myThirdUser)
 
+        //Seteamos el adaptador personalizado
         listView.adapter = MyUserAdapter(applicationContext, R.layout.item_list_view_user, userList)
         listView.emptyView = findViewById<TextView>(R.id.emptyElement) //Aquí seteamos la vista que queremos que aparezca en caso de que el listado esté vacio.
+
+        //Damos funcionalidad a cada uno de los items del listado añadiendoles su listener correspondiente.
+        listView.setOnItemClickListener { parent, view, position, id ->
+            var userName = userList.get(position).nombre
+            val infoToast = Toast.makeText(applicationContext,"Haciendo click en: $userName",Toast.LENGTH_LONG)
+            infoToast.show()
+        }
 
         //Array estandar (sin personalizar)
      /*   var frutas = arrayListOf<String>()
